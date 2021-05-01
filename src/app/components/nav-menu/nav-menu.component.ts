eba83cddfa59f4b95e9a2d5dc9d24d8c5073636b
Eduardo Cumpa Rojas <eduardo_crfin@outlook.com>
2021-04-30 21:34:14 -0500

    100%

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchMusicService } from 'src/app/services/search-music.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _searchMusicService: SearchMusicService) { }
 
   ngOnInit(): void {
   }
--- src/app/services/search-music.service.ts
+++ src/app/services/search-music.service.ts
@@ -6,23 +6,36 @@ import { Song } from '../interfaces/song';
 @Injectable({
   providedIn: 'root'
 })
+
 export class SearchMusicService {
-  private endpointURL = '/search';
+  private STATIC_URI = 'https://api.deezer.com'
+  private endpointURI = '/search';
+  private fullURL = '';
   public listResults: any | undefined;
   public infoSong: Song | undefined;
   public isPlayingAudio: boolean = false;
   isMute: boolean = false;
   public reproducir: HTMLAudioElement;
-  public albumCover:string= '../../../assets/images/no_music100x100.png';
+  public albumCover: string = 'assets/images/no_music100x100.png';
+  public logo: string = 'assets/images/foxbel-music.png';
+
+  isProd: boolean = false;
 
   constructor(private http: HttpClient) {
     this.reproducir = new Audio();
+
   }
 
   //Obtiene la canciones con el parámetro
   getResults(q: string): Observable<any> {
+    if (this.isProd) {
+      this.fullURL = this.STATIC_URI + this.endpointURI;
+    } else {
+      this.fullURL = this.endpointURI;
+    }
+    console.log("Llamando al endpoint: " + this.fullURL)
     const params = new HttpParams()
       .set('q', q);
-    return this.http.get(this.endpointURL, { params })
+    return this.http.get(this.fullURL, { params })
   }
 }
